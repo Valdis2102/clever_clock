bcd3520a71099f7bfc632d0a623e9a9672591dc6
Valdis2102 <102295101+Valdis2102@users.noreply.github.com>
2023-01-08 13:45:17 +0300

    Add files via upload

--- code/Pomodoro.js
+++ code/Pomodoro.js
@@ -6,14 +6,14 @@ break_sound.src = 'mp3/break.mp3'
 $('#clas_tim').click(showPomoTipe);
 $('#pomo_type').click(showClassicType);
 
-function showPomoTipe(){
+function showPomoTipe() {
     $('#clas_tim').addClass('hid');
     $('#classical_timer').addClass('hid');
     $('#pomo_type').removeClass('hid');
     $('#pomo_timer').removeClass('hid');
-}
+};
 
-function showClassicType(){
+function showClassicType() {
     $('#clas_tim').removeClass('hid');
     $('#classical_timer').removeClass('hid');
     $('#pomo_type').addClass('hid');
@@ -47,50 +47,50 @@ function startPomo(start, stop, pause) {
     stop.removeClass('hid');
     pause.attr('disabled', false);
     task_inp.attr('readonly', true);
-    break_inp.attr('readonly',true);
+    break_inp.attr('readonly', true);
     intervalStart = setInterval(funcTasckRun, 1000);
 };
 
-function funcTasckRun () {
+function funcTasckRun() {
     current_task_milis = ms($('#task_time')[0].innerText);
     current_break_time = ms($('#break_time')[0].innerText);
     console.log($('#break_time')[0].innerText);
 
-    if(pomo_run.hasClass('hid')){
-        if(current_task_milis > 0){
+    if (pomo_run.hasClass('hid')) {
+        if (current_task_milis > 0) {
             countdown(current_task_milis, task_vis, task_sound);
         };
-        if(current_task_milis == 0 && current_break_time > 0){
+        if (current_task_milis == 0 && current_break_time > 0) {
             countdown(current_break_time, break_vis, break_sound);
         };
-        if(current_task_milis <= 0 && current_break_time <= 0){
-            if(auto_pomo.is(':checked')){
+        if (current_task_milis <= 0 && current_break_time <= 0) {
+            if (auto_pomo.is(':checked')) {
                 stopPomo();
                 console.log('autorepeate ON');
                 startPomo(pomo_start, pomo_stop, pomo_pause, task_inp, break_inp);
-            }else{
-            stopPomo();
-            console.log('autorepeate OFF');
+            } else {
+                stopPomo();
+                console.log('autorepeate OFF');
             };
         };
     };
 };
 
-function countdown(milisec, vis, sound){
-        milisec -= 1000;
-        HH = Math.floor((milisec / 1000 / 60 / 60) % 24);
-        MM = Math.floor((milisec / 1000 / 60) % 60);
-        SS = (milisec / 1000) % 60;
-        if (HH < 10) { HH = "0" + HH };
-        if (MM < 10) { MM = "0" + MM };
-        if (SS < 10) { SS = "0" + SS };
-        let result = HH + ':' + MM + ':' + SS;
-        vis.html(result);
-        console.log(milisec);
-        if (milisec == 0){
-            sound.play();
-        };
-        };
+function countdown(milisec, vis, sound) {
+    milisec -= 1000;
+    HH = Math.floor((milisec / 1000 / 60 / 60) % 24);
+    MM = Math.floor((milisec / 1000 / 60) % 60);
+    SS = (milisec / 1000) % 60;
+    if (HH < 10) { HH = "0" + HH };
+    if (MM < 10) { MM = "0" + MM };
+    if (SS < 10) { SS = "0" + SS };
+    let result = HH + ':' + MM + ':' + SS;
+    vis.html(result);
+    console.log(milisec);
+    if (milisec == 0) {
+        sound.play();
+    };
+};
 
 function stopPomo() {
     pomo_stop.addClass('hid');
@@ -101,7 +101,7 @@ function stopPomo() {
     clearInterval(intervalStart);
     task_inp.attr('readonly', false);
     break_inp.attr('readonly', false);
-    setPomo(); 
+    setPomo();
 };
 
 function pausePomo() {
